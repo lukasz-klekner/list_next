46cb01f685eecfc01a6baad58ddff47ea0daae39
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-30 19:01:24 +0100

    adds some styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
@@ -7,15 +9,16 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      Homepage
-      <Navbar />
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
         accusantium id. Laboriosam corporis cumque, perspiciatis amet
         consequatur, optio, aliquam asperiores illum reiciendis nihil dolorem.
         Earum, ducimus. Perferendis sunt odit tempore.
       </p>
-      <Footer />
+      <Link href='/ninjas'>
+        <a className={styles.btn}>See Ninja Listing</a>
+      </Link>
     </div>
   )
 }
