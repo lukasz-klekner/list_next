3a8e7bd31d18cceabd2a137b7be203d4632796ba
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-30 17:35:28 +0100

    creates new pages

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cumque
+        beatae repudiandae, atque voluptates unde, fuga tenetur amet minima,
+        consequatur incidunt cum culpa. Sunt similique quaerat exercitationem
+        numquam. Aliquid, in?
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ pages/ninjas/index.js
@@ -0,0 +1,15 @@
+const Ninjas = () => {
+  return (
+    <div>
+      <h1>NInja list</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cumque
+        beatae repudiandae, atque voluptates unde, fuga tenetur amet minima,
+        consequatur incidunt cum culpa. Sunt similique quaerat exercitationem
+        numquam. Aliquid, in?
+      </p>
+    </div>
+  )
+}
+
+export default Ninjas
