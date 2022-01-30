178bc4438cd456a6050a7ce26b8dd1a3dadaa2cf
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-30 20:07:17 +0100

    adds Images and Head

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,22 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cumque
-        beatae repudiandae, atque voluptates unde, fuga tenetur amet minima,
-        consequatur incidunt cum culpa. Sunt similique quaerat exercitationem
-        numquam. Aliquid, in?
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cumque
+          beatae repudiandae, atque voluptates unde, fuga tenetur amet minima,
+          consequatur incidunt cum culpa. Sunt similique quaerat exercitationem
+          numquam. Aliquid, in?
+        </p>
+      </div>
+    </>
   )
 }
 
